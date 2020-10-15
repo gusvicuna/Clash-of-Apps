44338a8eb45da88918787207f9cfa9757b3baa63
Gus Night <gpvicuna@miuandes.cl>
2020-10-14 21:26:35 -0300

    feat(Country): add variables

--- Assets/Scripts/Game/Country/CountryBehaviour.cs
+++ Assets/Scripts/Game/Country/CountryBehaviour.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class CountryBehaviour : MonoBehaviour
 {
+    public int id;
+    public string countryName = "";
+
+    [HideInInspector]
+    public GameObject ownerApp;
+    [HideInInspector]
+    public GameObject[] influencers;
+
     // Start is called before the first frame update
-    void Start()
+    void Start() 
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void AttackCountry(int influencers, int defenderCountryId) {
+
+    }
+
+    public void AttackCountry(int influencers, int defenderCountryId, Dictionary<int,int> results) {
+
     }
 }
