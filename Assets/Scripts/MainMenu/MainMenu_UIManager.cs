76ced8df50fbe8e6ca08d6ce8bf7ce5fcdf7d499
Katherine <kjara@miuandes.cl>
2020-10-24 16:14:17 -0300

    more fixes in views

--- Assets/Scripts/MainMenu/MainMenu_UIManager.cs
+++ Assets/Scripts/MainMenu/MainMenu_UIManager.cs
@@ -10,6 +10,7 @@ public class MainMenu_UIManager : MonoBehaviour
     public GameObject main_menu_ui;
     public GameObject add_friend_ui;
     public GameObject stats_ui;
+    public GameObject all_friends_ui;
 
     private void Awake()
     {
@@ -30,17 +31,28 @@ public class MainMenu_UIManager : MonoBehaviour
         main_menu_ui.SetActive(true);
         add_friend_ui.SetActive(false);
         stats_ui.SetActive(false);
+        all_friends_ui.SetActive(false);
     }
     public void AddFriendScreen() // Regester button
     {
         main_menu_ui.SetActive(false);
         add_friend_ui.SetActive(true);
         stats_ui.SetActive(false);
+        all_friends_ui.SetActive(false);
     }
     public void StatsScreen()
     {
         main_menu_ui.SetActive(false);
         add_friend_ui.SetActive(false);
         stats_ui.SetActive(true);
+        all_friends_ui.SetActive(false);
+    }
+
+    public void AllFriendsScreen()
+    {
+        main_menu_ui.SetActive(false);
+        add_friend_ui.SetActive(false);
+        stats_ui.SetActive(false);
+        all_friends_ui.SetActive(true);
     }
 }
